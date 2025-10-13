ce8f21422998a4b740e1f4e3aa76fafd58360385
DKorablin2 <danila.korablin@unity3d.com>
2025-10-14 01:52:33 +0300

    Reverted back assembly attributes

--- Plugin.Configuration/Properties/AssemblyInfo.cs
+++ Plugin.Configuration/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: Guid("9160a0b6-1d31-4a8c-a256-eb1d933a29f4")]
 [assembly: System.CLSCompliant(true)]
+
+[assembly: AssemblyDescription("UI for Plugins configuration")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2012-2024")]
